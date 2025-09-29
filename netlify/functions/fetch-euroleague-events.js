4d060f9dd913cd68fb700e66b0e88039f5fd5d99
Damir Mikic <damir@pixupplay.com>
2025-09-29 10:20:19 +0200

    Create fetch-euroleague-events.js

---
+++ netlify/functions/fetch-euroleague-events.js
@@ -0,0 +1,47 @@
+exports.handler = async function(event, context) {
+  const API_KEY = process.env.API_KEY;
+
+  if (!API_KEY) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'API ključ nije konfigurisan u Netlify environment variables.' }),
+    };
+  }
+
+  const API_ENDPOINT = `https://sports-api.cloudbet.com/pub/v2/odds/competitions/basketball-international-euroleague?from=1759131849&to=1759563849&players=true&limit=100`;
+
+  try {
+    const response = await fetch(API_ENDPOINT, {
+      headers: {
+        "Accept": "application/json",
+        "X-API-Key": API_KEY
+      }
+    });
+
+    if (!response.ok) {
+      const errorBody = await response.text();
+      return {
+        statusCode: response.status,
+        body: JSON.stringify({ error: `API zahtev nije uspeo sa statusom ${response.status}: ${errorBody}` })
+      };
+    }
+
+    let data = await response.json();
+
+    if (data && data.events && Array.isArray(data.events)) {
+        const filteredEvents = data.events.filter(event => event.type !== 'EVENT_TYPE_OUTRIGHT');
+        data.events = filteredEvents;
+    }
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(data)
+    };
+  } catch (error) {
+    console.error("Greška pri preuzimanju:", error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify({ error: 'Nije uspelo preuzimanje podataka' })
+    };
+  }
+};
